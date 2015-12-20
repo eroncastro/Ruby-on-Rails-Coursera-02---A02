e9ab544d23f5db4874d8c6925b820b11ccfdfd35
Eron Castro <eroncastro@gmail.com>
2015-12-20 21:30:14 -0200

    Finished seeds and added default scope

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20151220203500) do
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
+  create_table "todo_items", force: :cascade do |t|
+    t.date     "due_date"
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "completed"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
+
+Profile.create!(
+  [
+    { first_name: 'Carly', last_name: 'Fiorina', birth_year: 1954, gender: 'female' },
+    { first_name: 'Donald', last_name: 'Trump', birth_year: 1946, gender: 'male' },
+    { first_name: 'Ben', last_name: 'Carson', birth_year: 1951, gender: 'male' },
+    { first_name: 'Hillary', last_name: 'Clinton', birth_year: 1947, gender: 'female' }
+  ]
+)
+
+Profile.find_each do |profile|
+  profile.create_user!(
+    username: profile.last_name,
+    password_digest: Digest::MD5.new.digest(profile.last_name).force_encoding('UTF-8')
+  )
+
+  user = User.last
+
+  TodoList.create!(
+    user: user,
+    list_name: user.username,
+    list_due_date: 1.year.from_now.to_date
+  )
+
+  5.times do |i|
+    TodoItem.create!(
+      todo_list: user.todo_lists.last,
+      due_date: 1.year.from_now.to_date,
+      title: "#{user.username}#{i}",
+      description: "#{user.username}#{i}",
+      completed: false,
+    )
+  end
+end
+
